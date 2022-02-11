5cc0e34bce55d2fc4d0778be6c24e9fca399273e
James Carlo Luchavez <jamescarlo.luchavez@fligno.com>
2022-02-11 13:47:54 +0800

    Set routes enabled to true by default.

--- config/starter-kit.php
+++ config/starter-kit.php
@@ -4,8 +4,8 @@ return [
     'override_exception_handler' => env('SK_OVERRIDE_EXCEPTION_HANDLER', false),
     'web_guard' => env('SK_WEB_GUARD', []),
     'api_guard' => env('SK_API_GUARD', []),
-    'enforce_morph_map' => env('SK_ENFORCE_MORPH_MAP', false),
+    'enforce_morph_map' => env('SK_ENFORCE_MORPH_MAP', true),
     'dynamic_relationships_enabled' => env('SK_DYNAMIC_RELATIONSHIP_ENABLED', true),
-    'routes_enabled' => env('SK_ROUTES_ENABLED', false),
+    'routes_enabled' => env('SK_ROUTES_ENABLED', true),
     'verify_ssl' => env('SK_VERIFY_SSL', true),
 ];
